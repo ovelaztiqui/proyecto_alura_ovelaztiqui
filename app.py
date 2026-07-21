4a52c2b7078c604df9090871422acafac205c642
ovelaztiqui <Oscar.velaztiqui@itti.digital>
2026-07-21 18:14:11 -0300

    fix: parche de compatibilidad para sqlite3 con chromadb

--- app.py
+++ app.py
@@ -1,3 +1,10 @@
+try:
+    __import__('pysqlite3')
+    import sys
+    sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
+except ImportError:
+    pass
+
 import streamlit as st
 from chatbot import generar_respuesta
 
